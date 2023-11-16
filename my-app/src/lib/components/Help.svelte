2cf687cde69d1d5181a3d0a858d863c36fcbc2a9
Ivar Schuyt <112855849+IvarSchuyt@users.noreply.github.com>
2023-11-16 22:35:53 +0100

    first component

---
+++ my-app/src/lib/components/Help.svelte
@@ -0,0 +1,50 @@
+<script>
+
+</script>
+
+<section>
+
+    <div>
+    <h3>Vragen over uw betalingen?</h3>
+        <p>Onze klantenservice is bereikbaar op werkdagen van 9.00 tot 17.00 uur. </p>
+        <p>06-12345678</p>
+        <p>janjansen@email.nl</p>
+    </div>
+    <button>venster sluiten</button>
+
+</section>
+
+<style>
+
+    section{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        max-width: 33vw;
+        height: 50vh;
+        max-height: 50vh;
+        margin-top: 25vh;
+        border: solid black 0.25rem;
+    }
+
+    p{
+        padding: 0 1rem;
+        font-size: 1rem;
+    }
+
+    p:nth-of-type(2){
+        margin-top: 2rem;
+    }
+
+    h3{
+        padding: 1rem 2rem 0rem 1rem;
+        margin: 0 0 5rem 0;
+    }
+
+    button{
+        border: none;
+        background: transparent;
+        padding: 0 0 1rem 0;
+    }
+
+</style>
---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+
+ import Help from '$lib/components/Help.svelte';
+
+</script>
+
+<main>
+
+    <Help/>
+
+</main>
+
+<style>
+
+    main{
+        display: flex;
+        justify-content: flex-end;
+    }
+
+</style>
